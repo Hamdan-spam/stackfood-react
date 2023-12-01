4c29c431aa20e6a758fdfc773b9a1fe385c80c48
Hamdan-Khan <hamdankhan212@gmail.com>
2023-12-01 20:02:13 +0500

    Commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
     reactStrictMode: true,
     images: {
         domains: ['stackfood.6am.one'],
-        staticPageGenerationTimeout: 1500//Domain of image host
+        staticPageGenerationTimeout: 1500, //Domain of image host
     },
 }
